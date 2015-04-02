cfillion/bows

commit ea2277933f52a6cbc0b210791ade29106479082f

add a separator below the last message when switching to another tab

src/layout.coffee
@@ -52,10 +52,7 @@ class Layout extends EventEmitter
       return
 
     page.on 'alertCountChanged', (count) =>
-      if index != @currentIndex
-        @tabbar.setAlertCount index, count
-      else
-        page.resetAlerts()
+      @tabbar.setAlertCount index, count
 
       return
 
@@ -73,15 +70,13 @@ class Layout extends EventEmitter
     return
 
   currentTabChanged: (newIndex) ->
-    return unless page = @pages[newIndex]
+    return if @currentIndex == newIndex
 
-    if oldPage = @pages[@currentIndex]
-      Utils.removeClass 'current', oldPage.node
-
-    Utils.addClass 'current', page.node
-    page.focus()
+    @pages[@currentIndex]?.blur() # previous page
+    @pages[newIndex].focus()
 
     @currentIndex = newIndex
+
     return
 
   closePage: (index) ->

src/page.coffee
@@ -23,6 +23,7 @@ class Page extends EventEmitter
     @setName identifier
 
     @alertCount = 0
+    @hasFocus = false
 
     window.addEventListener 'resize', => @onResize()
 
@@ -46,10 +47,20 @@ class Page extends EventEmitter
     return
 
   focus: ->
+    Utils.addClass 'current', @node
     Utils.scrollToBottom @messages
-    @resetAlerts()
 
+    @resetAlerts()
     @input.focus()
+    @hasFocus = true
+
+    return
+
+  blur: ->
+    Utils.removeClass 'current', @node
+
+    @hasFocus = false
+    @addSeparator()
 
     return
 
@@ -85,7 +96,13 @@ class Page extends EventEmitter
     @messages.appendChild container
     Utils.scrollToBottom @messages if wasAtBottom
 
-    @alert()
+    @alert() unless @hasFocus
+
+    return
+
+  addSeparator: ->
+    separator = Utils.createNode 'hr'
+    @messages.appendChild separator
 
     return
 

style/_page.sass
@@ -20,6 +20,13 @@
   overflow-y: scroll
 
   text-shadow: 1px 1px darken($main-background, 5%)
+  padding-right: 6px
+
+  hr
+    border-color: $dark-foreground
+
+  hr:not(:last-of-type), hr:first-child, hr:last-child
+    display: none
 
 .#{$prefix}message
   margin: 0

commit e5fd870abe02bfc6d060f29a326c9a2ac4631836

fix tab switch after closing another one (broken since ea227793)

src/layout.coffee
@@ -87,7 +87,7 @@ class Layout extends EventEmitter
     @container.removeChild page.node
 
     # don't resize the array
-    @pages[index] = false
+    @pages[index] = null
 
     @emit 'pageClosed', page
 